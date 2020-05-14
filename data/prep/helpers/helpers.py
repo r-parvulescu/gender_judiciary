a388efafc123d71baecba04b6b9aac21e54130dd
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-05-14 14:37:18 -0400

    added function print_full_names_ldist_apart, new parameter in function pairwise_ldist

--- data/prep/helpers/helpers.py
+++ data/prep/helpers/helpers.py
@@ -2,8 +2,7 @@
 Handy helpers.
 """
 
-import csv
-import PyICU
+import pandas as pd
 import Levenshtein
 
 
@@ -21,29 +20,36 @@ def deduplicate_list_of_lists(list_of_lists):
     return [row.split('|') for row in uniques]
 
 
-def pairwise_ldist(strings_iter, lev_dist):
+def pairwise_ldist(strings_iter, lev_dist, sort_key=None):
     """
     :param strings_iter: iterable (e.g. set, list) of strings
     :param lev_dist: int indicating the desired Levenshtein distance
+    :param sort_key: the key for sorting the list of tuples; if None, sorts by first tuple entry
     :return list of 2-tuples of full names lev_dist apart, alphabetically sorted by first name in tuple
     NB: pairwise comparison is lower triangular, no diagonals
      """
-    return sorted(list(filter(None, [(x, y) if 0 < Levenshtein.distance(x, y) <= lev_dist else ()
-                                     for i, x in enumerate(strings_iter)
-                                     for j, y in enumerate(strings_iter) if i > j])))
+
+    list_of_tuples_ldist_apart = list(filter(None, [(x, y) if 0 < Levenshtein.distance(x, y) <= lev_dist else ()
+                                                    for i, x in enumerate(strings_iter)
+                                                    for j, y in enumerate(strings_iter) if i > j]))
+
+    if sort_key is None:
+        return sorted(list_of_tuples_ldist_apart)
+    else:
+        return sorted(list_of_tuples_ldist_apart, key=sort_key)
 
 
-def print_uniques(csv_file, col_idx):
-    """print all unique column entries, helps weed out typos, misspellings, etc. in names"""
-    with open(csv_file, 'r') as f:
-        reader = csv.reader(f)
-        next(reader, None)  # skip head
-        uniques = set()
-        for row in reader:
-            uniques.add(row[col_idx])
+def print_full_names_ldist_apart(csv_file_path, l_dist):
+    """
+    Prints out a sorted column of all full names that are ldist or more apart in terms of Levenshtein distance.
+    This helps weed out typos by hand that are too subtle to leave to automated functions.
+    :param csv_file_path: string, file path to a csv file
+    :param l_dist: int, maximum Levenshtein/edit distance between two full names that you want to compare
+    :return: None
+    """
+    df = pd.read_csv(csv_file_path)
+    table = df.values.tolist()
+    unique_full_names = {row[1] + ' | ' + row[2] for row in table}  # row[1] = surnames, row[2] = given names
 
-    # sort taking into account Romanian diacritics
-    collator = PyICU.Collator.createInstance(PyICU.Locale('ro_RO.UTF-8'))
-    uniques = [i for i in sorted(list(uniques), key=collator.getSortKey)]
-    for u in sorted(list(uniques)):
-        print(u)
+    full_name_ldist = pairwise_ldist(unique_full_names, l_dist)
+    [print(full_name_pair) for full_name_pair in full_name_ldist]
